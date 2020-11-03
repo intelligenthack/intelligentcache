7a4bc5a8463df0bed32069566c6145897b6db8bf
ocoster-is <59826626+ocoster-is@users.noreply.github.com>
2020-11-03 12:19:55 +0000

    Update protobuf serializer to use compression

--- IntelligentCache/ProtobufSerializer.cs
+++ IntelligentCache/ProtobufSerializer.cs
@@ -1,24 +1,61 @@
-using StackExchange.Redis;
-using System;
 using ProtoBuf;
+using StackExchange.Redis;
 using System.IO;
+using System.IO.Compression;
+using System.ServiceModel.Channels;
 
 namespace IntelligentHack.IntelligentCache
 {
     public class ProtobufSerializer : IRedisSerializer
     {
+        public CompressionFormat CompressionFormat { get; set; } = CompressionFormat.GZip;
+
         public T Deserialize<T>(RedisValue value)
         {
-            return Serializer.Deserialize<T>(value);
+            switch (CompressionFormat)
+            {
+                case CompressionFormat.Deflate:
+                    using (var stream = new DeflateStream(new MemoryStream(value), CompressionMode.Decompress))
+                    {
+                        return Serializer.Deserialize<T>(stream);
+                    }
+                case CompressionFormat.GZip:
+                    using(var stream = new GZipStream(new MemoryStream(value), CompressionMode.Decompress))
+                    {
+                        return Serializer.Deserialize<T>(stream);
+                    }
+                case CompressionFormat.None:
+                default:
+                    return Serializer.Deserialize<T>(value);
+            }
         }
 
         public RedisValue Serialize<T>(T instance)
         {
-            using(var memStream = new MemoryStream())
+            switch (CompressionFormat)
             {
-                Serializer.Serialize(memStream, instance);
-                return RedisValue.CreateFrom(memStream);
-            }
+                case CompressionFormat.Deflate:
+                    using (var memStream = new MemoryStream())
+                    using (var stream = new DeflateStream(memStream, CompressionLevel.Optimal))
+                    {
+                        Serializer.Serialize(stream, instance);
+                        return RedisValue.CreateFrom(memStream);
+                    }
+                case CompressionFormat.GZip:
+                    using (var memStream = new MemoryStream())
+                    using (var stream = new GZipStream(memStream, CompressionLevel.Optimal))
+                    {
+                        Serializer.Serialize(stream, instance);
+                        return RedisValue.CreateFrom(memStream);
+                    }
+                case CompressionFormat.None:
+                default:
+                    using (var stream = new MemoryStream())
+                    {
+                        Serializer.Serialize(stream, instance);
+                        return RedisValue.CreateFrom(stream);
+                    }
+            };
         }
     }
 }
