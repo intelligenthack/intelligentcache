6260f1d978dd4e5a5e05628655b31f368f4818d9
gabrieldevoto <gabrielalejandrodevoto@gmail.com>
2020-11-04 15:45:22 -0300

    Update PassThroughCache.cs

--- IntelligentCache/PassThroughCache.cs
+++ IntelligentCache/PassThroughCache.cs
@@ -9,9 +9,17 @@ namespace IntelligentHack.IntelligentCache
     /// </summary>
     public sealed class PassThroughCache : ICache
     {
+        public TimeSpan CacheDuration { get; set; }
+
+        public PassThroughCache()
+        {
+            this.CacheDuration = TimeSpan.MaxValue;
+        }
+
         public T GetSet<T>(string key, Func<T> calculateValue, TimeSpan duration) where T : class
         {
             return calculateValue();
+
         }
 
         public ValueTask<T> GetSetAsync<T>(string key, Func<CancellationToken, ValueTask<T>> calculateValue, TimeSpan duration, CancellationToken cancellationToken = default) where T : class
@@ -27,6 +35,16 @@ namespace IntelligentHack.IntelligentCache
         public ValueTask InvalidateAsync(string key)
         {
             return new ValueTask();
+        }
+
+        public ValueTask<T> GetSetAsync<T>(string key, Func<CancellationToken, ValueTask<T>> calculateValue, CancellationToken cancellationToken = default) where T : class
+        {
+            return this.GetSetAsync(key,calculateValue,this.CacheDuration,cancellationToken);
+        }
+
+        public T GetSet<T>(string key, Func<T> calculateValue) where T : class
+        {
+            return this.GetSet(key,calculateValue,this.CacheDuration);
         }
     }
 }
